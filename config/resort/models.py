08959ad07c0468b55ca0610a6c34af057364ce5d
Dmitriy Fomenko <d.fomenko95@gmail.com>
2025-12-19 17:31:07 +0500

    Создал модель TripMedia и применил миграции

--- config/resort/models.py
+++ config/resort/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Resort(models.Model):
     """Модель справочник, к которому привязаны поездки."""
     name = models.CharField(max_length=150)
@@ -26,3 +27,11 @@ class Trip(models.Model):
         return f"{self.user.username} - {self.resort.name}"
 
 
+class TripMedia(models.Model):
+    """Модель для хранения медиафайлов, связанных с поездкой."""
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='media')
+    image = models.ImageField(upload_to='trip_photos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Media for trip {self.trip.id}"
