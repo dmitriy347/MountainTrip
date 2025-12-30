ccb4ed800c3e5bd4c493251926fdeb53b72e56ec
Dmitriy Fomenko <d.fomenko95@gmail.com>
2025-12-30 09:47:22 +0500

    Исправил logout из системы в файле base.html (не работал из-за отправки GET-запроса). В base.html эта кнопка доступна через "<form method="post"..."

--- config/users/urls.py
+++ config/users/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('sign-in/', views.UserLoginView.as_view(), name='login'),
-    path('sing-out/', views.UserLogoutView.as_view(), name='logout'),
+    path('sign-out/', views.UserLogoutView.as_view(), name='logout'),
     path('sign-up/', views.UserRegisterView.as_view(), name='register'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
