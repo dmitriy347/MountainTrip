8570c7429896a413818846db3fdaba248e6c9713
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-13 13:49:08 +0500

    Добавил cброс кеша при изменении курорта через signals.py

--- config/resort/signals.py
+++ config/resort/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_delete
+from django.core.cache import cache
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from .models import TripMedia
+from .models import TripMedia, Resort
 
 
 @receiver(post_delete, sender=TripMedia)
@@ -8,3 +9,8 @@ def delete_trip_media_file(sender, instance, **kwargs):
     """Удаление файла изображения из файловой системы при удалении объекта TripMedia."""
     if instance.image:
         instance.image.delete(save=False)
+
+@receiver([post_save, post_delete], sender=Resort)
+def clear_resort_cache(sender, **kwargs):
+    """Очистка кэша при сохранении или удалении объекта Resort."""
+    cache.delete('resort_list')
