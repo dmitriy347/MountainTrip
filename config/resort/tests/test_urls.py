3cfa0e5da16ee6102ebc995063fce51b66a59735
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-14 17:32:32 +0500

    Закончил с тестированием test_urls.py

--- config/resort/tests/test_urls.py
+++ config/resort/tests/test_urls.py
@@ -4,53 +4,79 @@ from django.urls import reverse, resolve
 from resort import views
 
 
-@pytest.mark.django_db
-def test_home_url():
+def test_home_url_resolves():
     """Тестирование URL главной страницы"""
     url = reverse('home')
-    assert url == '/'   # Проверяем, что URL для имени 'home' равен '/'
     match = resolve(url)
-    assert match.func == views.index  # Проверяем, что URL разрешается в правильное представление
+    assert match.func is views.index  # Проверяем, что URL разрешается в правильное представление
 
 
 @pytest.mark.django_db
-def test_resort_detail_url(resort):
+def test_resort_detail_url_resolves(resort):
     """Тестирование URL страницы курорта"""
     url = reverse('resort_detail', kwargs={'resort_slug': resort.slug})
-    assert url == f'/resort/{resort.slug}'  # Проверяем, что URL для имени 'resort_detail' корректен
     match = resolve(url)
-    assert match.func.view_class == views.ResortDetailView  # Проверяем, что URL разрешается в правильное представление
+    assert match.func.view_class is views.ResortDetailView
 
 
-@pytest.mark.django_db
-def test_resort_list_urls():
-    """Тестирование URL для списка курортов и поездок"""
+def test_resort_list_url_resolves():
+    """Тестирование URL для списка курортов"""
     url = reverse('resort_list')
-    assert url == '/resorts/'  # Проверяем URL для списка курортов
     match = resolve(url)
-    assert match.func.view_class == views.ResortListView  # Проверяем представление для
+    assert match.func.view_class is views.ResortListView
 
 
 @pytest.mark.django_db
-def test_trip_list_url():
+def test_trip_detail_url_resolves(trip):
+    """Тестирование URL страницы поездки"""
+    url = reverse('trip_detail', kwargs={'trip_id': trip.id})
+    match = resolve(url)
+    assert match.func.view_class is views.TripDetailView
+
+
+def test_trip_list_url_resolves():
     """Тестирование URL для списка поездок"""
     url = reverse('trip_list')
-    assert url == '/trips/'  # Проверяем URL для списка поездок
     match = resolve(url)
-    assert match.func.view_class == views.TripListView  # Проверяем представление для списка поездок
+    assert match.func.view_class is views.TripListView
+
+
+def test_trip_create_url_resolves():
+    """Тестирование URL для создания поездки"""
+    url = reverse('trip_create')
+    match = resolve(url)
+    assert match.func.view_class is views.TripCreateView
 
 
 @pytest.mark.django_db
-def test_trip_urls_reverse_and_resolve():
-    assert reverse('trip_list') == '/trips/'
-    assert reverse('trip_create') == '/trips/create'
-    assert reverse('trip_detail', kwargs={'trip_id': 1}) == '/trip/1'
-    assert reverse('trip_edit', kwargs={'trip_id': 1}) == '/trips/1/edit'
-    assert reverse('trip_delete', kwargs={'trip_id': 1}) == '/trips/1/delete'
-    assert reverse('trip_media_add', kwargs={'trip_id': 1}) == '/trips/1/media/add'
-    assert reverse('trip_media_delete', kwargs={'media_id': 1}) == '/media/1/delete/'
+def test_trip_edit_url_resolves(trip):
+    """Тестирование URL для редактирования поездки"""
+    url = reverse('trip_edit', kwargs={'trip_id': trip.id})
+    match = resolve(url)
+    assert match.func.view_class is views.TripUpdateView
 
-    assert resolve('/trip/1').func.view_class is views.TripDetailView
-    assert resolve('/trips/1/edit').func.view_class is views.TripUpdateView
-    assert resolve('/trips/1/media/add').func.view_class is views.TripMediaAddView
-    assert resolve('/media/1/delete/').func.view_class is views.TripMediaDeleteView
+
+@pytest.mark.django_db
+def test_trip_delete_url_resolves(trip):
+    """Тестирование URL для удаления поездки"""
+    url = reverse('trip_delete', kwargs={'trip_id': trip.id})
+    match = resolve(url)
+    assert match.func.view_class is views.TripDeleteView
+
+
+@pytest.mark.django_db
+def test_trip_media_add_url_resolves(trip):
+    """Тестирование URL для добавления медиафайла к поездке"""
+    url = reverse('trip_media_add', kwargs={'trip_id': trip.id})
+    match = resolve(url)
+    assert match.func.view_class is views.TripMediaAddView
+
+
+@pytest.mark.django_db
+def test_trip_media_delete_url_resolves(trip_media):
+    """Тестирование URL для удаления медиафайла поездки"""
+    url = reverse('trip_media_delete', kwargs={'media_id': trip_media.id})
+    match = resolve(url)
+    assert match.func.view_class is views.TripMediaDeleteView
+
+
