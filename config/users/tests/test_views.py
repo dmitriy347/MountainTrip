db97d2d06c6ff17eefaf566cf8177da9457e5e83
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-16 16:18:27 +0500

    add: добавил файлы в пакет users/tests

---
+++ config/users/tests/test_views.py
@@ -0,0 +1,9 @@
+import pytest
+from django.urls import reverse
+
+
+# @pytest.mark.django_db
+# def test_user_login_view_returns_200(client):
+#     url = reverse('users:login')
+#     response = client.get(url)
+#     assert response.status_code == 200
