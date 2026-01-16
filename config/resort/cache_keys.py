b7793d5e7a1bb727031ff3b83bcbe4b16dc3f1b3
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-16 17:50:05 +0500

    style(resort): cache_keys.py

--- config/resort/cache_keys.py
+++ config/resort/cache_keys.py
@@ -4,7 +4,6 @@
 
 class CacheKeys:
     """Класс для генерации ключей кэша."""
-
     # Префиксы для разных типов данных
     PREFIX_RESORT = 'resort'
     PREFIX_TRIP = 'trip'
@@ -16,8 +15,8 @@ class CacheKeys:
         """Генерирует ключ кэша для счетчиков поездок курорта по его ID."""
         return f'{CacheKeys.PREFIX_RESORT}:{resort_id}:trips_counts'
 
+
 class CacheTimeouts:
     """Класс для хранения таймаутов кэша в секундах."""
-
     RESORT_LIST = 60 * 10  # 10 минут
     RESORT_TRIPS_COUNTS = 60 * 10  # 10 минут
