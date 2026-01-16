7a4da8ea0a3b7d547c9c52e16388fb491025eb16
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-16 17:38:28 +0500

    feat(resort): добавил cache_keys.py

---
+++ config/resort/cache_keys.py
@@ -0,0 +1,23 @@
+"""Централизованное управление ключами кэша.
+Все ключи кэша проекта хранятся здесь.
+"""
+
+class CacheKeys:
+    """Класс для генерации ключей кэша."""
+
+    # Префиксы для разных типов данных
+    PREFIX_RESORT = 'resort'
+    PREFIX_TRIP = 'trip'
+
+    RESORT_LIST = f'{PREFIX_RESORT}:list'
+
+    @staticmethod
+    def resort_trips_counts(resort_id):
+        """Генерирует ключ кэша для счетчиков поездок курорта по его ID."""
+        return f'{CacheKeys.PREFIX_RESORT}:{resort_id}:trips_counts'
+
+class CacheTimeouts:
+    """Класс для хранения таймаутов кэша в секундах."""
+
+    RESORT_LIST = 60 * 10  # 10 минут
+    RESORT_TRIPS_COUNTS = 60 * 10  # 10 минут
