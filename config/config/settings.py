330608da2898abfa74fb6972b627d0069b27fb05
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-19 13:51:45 +0500

    chore: ALLOWED_HOSTS

--- config/config/settings.py
+++ config/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INTERNAL_IPS = [
     '127.0.0.1',
