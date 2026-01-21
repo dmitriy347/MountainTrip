a4e5038e557f43fa7b2414e6055f728616e4d16e
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-21 13:02:06 +0500

    test: удалил фикстуру social_app

--- config/users/tests/conftest.py
+++ config/users/tests/conftest.py
@@ -21,24 +21,3 @@ def auth_client(client, user):
     """Django test client с авторизованным пользователем"""
     client.login(username='testuser', password='testpass123')
     return client
-
-
-@pytest.fixture
-def social_app(db):
-    """
-    Создаёт SocialApp для GitHub OAuth (нужно для django-allauth).
-    Без этого шаблоны с {% provider_login_url %} будут падать.
-    """
-    try:
-        site = Site.objects.get_current()
-        app = SocialApp.objects.create(
-            provider='github',
-            name='GitHub',
-            client_id='test-client-id',
-            secret='test-secret',
-        )
-        app.sites.add(site)
-        return app
-    except ImportError:
-        # allauth не установлен в тестах
-        return None
