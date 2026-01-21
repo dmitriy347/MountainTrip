8a28c8acfa9d05f964d9a336c9972a1dfcae7aad
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-01-21 10:21:24 +0500

    feat: указал в settings.py директорию для хранения фикстур

--- config/config/settings.py
+++ config/config/settings.py
@@ -194,3 +194,7 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+FIXTURE_DIRS = [
+    BASE_DIR / "fixtures",
+]
