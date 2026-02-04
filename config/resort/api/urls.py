4e287ca53a7fe5f648ff84ab4de4f4260f4f97be
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-04 15:34:31 +0500

    feat(resort/api): добавил роутер media в urls.py

--- config/resort/api/urls.py
+++ config/resort/api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ResortViewSet, TripViewSet
+from .views import ResortViewSet, TripViewSet, TripMediaViewSet
 
 router = DefaultRouter()
 router.register(r"resorts", ResortViewSet, basename="resort")
 router.register(r"trips", TripViewSet, basename="trip")
+router.register(r"media", TripMediaViewSet, basename="tripmedia")
 
 urlpatterns = router.urls
