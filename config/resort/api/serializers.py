99504967335efd81bced815e9ec5d982d2cbd2db
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-04 10:53:04 +0500

    feat(resort/api): Добавил TripSerializer

--- config/resort/api/serializers.py
+++ config/resort/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from resort.models import Resort
+from resort.models import Resort, Trip
 
 
 class ResortSerializer(serializers.ModelSerializer):
@@ -9,3 +9,24 @@ class ResortSerializer(serializers.ModelSerializer):
         model = Resort
         fields = ["id", "name", "slug", "region", "description", "created_at"]
         read_only_fields = ["id", "slug", "created_at"]
+
+
+class TripSerializer(serializers.ModelSerializer):
+    """Serializer для модели Trip."""
+
+    user = serializers.StringRelatedField()
+    resort = ResortSerializer(read_only=True)
+
+    class Meta:
+        model = Trip
+        fields = [
+            "id",
+            "user",
+            "resort",
+            "start_date",
+            "end_date",
+            "comment",
+            "is_public",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
