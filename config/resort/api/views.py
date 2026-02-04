3b4271a517773e629bc73dd35a845924b94b2a10
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-04 14:55:01 +0500

    feat(resort/api): Добавил TripViewSet с правами доступа

--- config/resort/api/views.py
+++ config/resort/api/views.py
@@ -1,6 +1,9 @@
-from resort.models import Resort
+from django.db.models import Q
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from resort.models import Resort, Trip
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from .serializers import ResortSerializer
+from .serializers import ResortSerializer, TripSerializer
 from rest_framework.filters import OrderingFilter
 
 
@@ -20,3 +23,34 @@ class ResortViewSet(ReadOnlyModelViewSet):
         if region:
             queryset = queryset.filter(region__icontains=region)
         return queryset
+
+
+class TripViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet для модели Trip.
+
+    Права доступа:
+    - Неавторизованные: только чтение (GET) публичных поездок
+    - Авторизованные: чтение всех + в будущем CRUD своих поездок
+    """
+
+    serializer_class = TripSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Гости - только GET, авторизованные - CRUD
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["start_date", "end_date"]  # Разрешенные поля для сортировки
+    ordering = ["-start_date"]  # Сортировка по умолчанию
+
+    def get_queryset(self):
+        """Фильтрация поездок в зависимости от авторизации пользователя."""
+        user = self.request.user
+        if user.is_authenticated:
+            # Авторизованный: публичные + свои приватные
+            return Trip.objects.filter(
+                Q(is_public=True) | Q(user=user)
+            ).select_related("user", "resort").prefetch_related("media")
+        else:
+            # Гость: только публичные
+            return Trip.objects.filter(is_public=True).select_related(
+                "user", "resort"
+            ).prefetch_related("media")
