d4ac792dec5025d3edd05b0b09bd5b4baabc4f63
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-04 10:37:37 +0500

    feat(resort/api): Добавил сортировку и фильтрацию по региону

--- config/resort/api/views.py
+++ config/resort/api/views.py
@@ -1,6 +1,7 @@
 from resort.models import Resort
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .serializers import ResortSerializer
+from rest_framework.filters import OrderingFilter
 
 
 class ResortViewSet(ReadOnlyModelViewSet):
@@ -9,3 +10,13 @@ class ResortViewSet(ReadOnlyModelViewSet):
     queryset = Resort.objects.all()
     serializer_class = ResortSerializer
     lookup_field = "slug"
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["name", "region"]  # Разрешенные поля для сортировки
+    ordering = ["name"]  # Сортировка по умолчанию
+
+    def get_queryset(self):
+        queryset = Resort.objects.all()
+        region = self.request.query_params.get("region")
+        if region:
+            queryset = queryset.filter(region__icontains=region)
+        return queryset
