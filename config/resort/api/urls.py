029954e340bc40646d9fd99925dac212d6e22743
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-04 17:48:42 +0500

    feat(resort/api): добавил роутер users в urls.py

--- config/resort/api/urls.py
+++ config/resort/api/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ResortViewSet, TripViewSet, TripMediaViewSet
+from .views import ResortViewSet, TripViewSet, TripMediaViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r"resorts", ResortViewSet, basename="resort")
 router.register(r"trips", TripViewSet, basename="trip")
 router.register(r"media", TripMediaViewSet, basename="tripmedia")
+router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = router.urls
