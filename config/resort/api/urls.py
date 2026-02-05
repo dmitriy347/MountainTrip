ecda8019621ecda992827e55b1cb5ed1d172ea60
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-05 15:02:20 +0500

    feat(resort/api): создал эндпоинты для токенов в urls.py

--- config/resort/api/urls.py
+++ config/resort/api/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import ResortViewSet, TripViewSet, TripMediaViewSet, UserViewSet
 
 router = DefaultRouter()
@@ -7,4 +10,9 @@ router.register(r"trips", TripViewSet, basename="trip")
 router.register(r"media", TripMediaViewSet, basename="tripmedia")
 router.register(r"users", UserViewSet, basename="user")
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    # JWT эндпоинты
+    path("auth/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+] + router.urls
