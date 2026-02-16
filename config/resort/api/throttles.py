4b30d3d84eb0ef263cb5ddc8fe971eb2e8fb21e5
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-02-16 11:14:55 +0500

    feat: добавил кастомный throttle в throttles.py для JWN токенов

---
+++ config/resort/api/throttles.py
@@ -0,0 +1,9 @@
+from rest_framework.throttling import AnonRateThrottle
+
+
+class AuthThrottle(AnonRateThrottle):
+    """
+    Ограничение попыток получения JWT токена
+    Защита от bruteforce атак
+    """
+    scope = 'auth'  # Имя кастомного лимита
